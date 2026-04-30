2fc3256a71f168c2b684fec1db02eff6ac681edc
Esai Flores <flores.esaijonathan@gmail.com>
2026-04-30 16:19:46 -0500

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+
+        return nums.indexOf(target);
+    }
+}
